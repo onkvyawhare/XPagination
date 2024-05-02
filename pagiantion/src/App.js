3709d909bd2a560b1092b1d22a082c7b2e95adc7
onkvyawhare <onkvyawhare@gmail.com>
2024-05-02 23:33:50 +0530

    final commit

--- pagiantion/src/App.js
+++ pagiantion/src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
 
 function App() {
+
+  const [members, setMembers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+useEffect(()=>{
+  fetchMembers();
+},[currentPage]);
+
+  const fetchMembers = async () => {
+    try {
+      const response = await fetch(
+        "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const data = await response.json();
+      setMembers(data);
+      setTotalPages(Math.ceil(data.length / 10));
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      alert("Failed to fetch data");
+    }
+  };
+
+  const handlePrevious = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const handleNext = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
+  const startIndex=(currentPage-1)*10;
+  const endIndex=Math.min(startIndex+10,members.length);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h2> Employee Data Table</h2>
+     <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+        </tr>
+      </thead>
+      <tbody>
+        {members.slice(startIndex,endIndex).map((member)=>(
+          <tr key={member.id}>
+            <td>{member.id}</td>
+            <td>{member.name}</td>
+            <td>{member.email}</td>
+            <td>{member.role}</td>
+          </tr>
+        ))}
+      </tbody>
+     </table>
+
+     <div className="pagination">
+        <button onClick={handlePrevious} disabled={currentPage === 1}>
+          Previous
+        </button>
+        <span class="page">{currentPage}</span>
+        <button onClick={handleNext} disabled={currentPage === totalPages}>
+          Next
+        </button>
+      </div>
     </div>
+    
   );
 }
 
